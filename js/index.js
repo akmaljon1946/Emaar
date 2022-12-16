8aed6e20fceb9e98837959927ac5e8cf84a794f4
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-12-16 17:59:03 +0500

    phone number

--- js/index.js
+++ js/index.js
@@ -15,3 +15,11 @@ window.onclick = function (event) {
 		myModal.style.display = 'none'
 	}
 }
+
+const hideTel = document.querySelector('#hideTel')
+const showTel = document.querySelector('#showTel')
+
+showTel.addEventListener('click', () => showSTR('777-69-69'))
+
+const showSTR = trueTel =>
+	hideTel.firstChild.replaceData(6, trueTel.length, trueTel)
